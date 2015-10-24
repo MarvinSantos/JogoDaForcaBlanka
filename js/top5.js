026c4f03e7a57d0f4e64a4912d87a0bad283e766
Marvin Santos <marvincenteno23@gmail.com>
2015-10-24 15:28:19 -0200

    script para listar o top5

--- js/top5.js
+++ js/top5.js
@@ -1,11 +1,22 @@
-$(".button-top5").click(function(){
+$(".btnTopFive").click(function(){
   listarTop5();
-}
+});
 function listarTop5() {
-  $.get().done(function (elem) {
-    elem.forEach(function () {
-      var arraynomes=[];
-      arraynomes.add({nome : elem.nome pontuacao : elem.pontuacao});
+  $.get('http://localhost:3000/pessoas').done(function (elem) {
+    $('#pessoa').empty();
+    var rankingOrdenado = [];
+    var arrayTop5 = [];
+    
+    rankingOrdenado = elem.sort(function(elemEsq, elemDir){
+      return elemEsq.pontos < elemDir.pontos;
+    });
+
+    arrayTop5 = rankingOrdenado.slice(0,5);
+    $('#listaDoTop5').addClass('lista');
+    arrayTop5.forEach(function (elem2) {
+      $('#pessoa').append(
+        $('<li>').html(elem2.nome + '- Pontuação: ' + elem2.pontos)
+      );
     });
   });
-}
+};
