f2d9da4985e1d6ed64620f781184d3129bc04af4
Marvin Santos <marvincenteno23@gmail.com>
2015-10-24 21:15:34 -0200

    ajustes

--- js/top5.js
+++ js/top5.js
@@ -3,18 +3,18 @@ $(".btnTopFive").click(function(){
 });
 function listarTop5() {
   $.get('http://localhost:3000/pessoas').done(function (elem) {
-    $('#pessoa').empty();
+    $('.pessoa').empty();
     var rankingOrdenado = [];
     var arrayTop5 = [];
-    
+
     rankingOrdenado = elem.sort(function(elemEsq, elemDir){
       return elemEsq.pontos < elemDir.pontos;
     });
 
     arrayTop5 = rankingOrdenado.slice(0,5);
-    $('#listaDoTop5').addClass('lista');
+    $('.listaDoTop5').addClass('lista');
     arrayTop5.forEach(function (elem2) {
-      $('#pessoa').append(
+      $('.pessoa').append(
         $('<li>').html(elem2.nome + '- Pontuação: ' + elem2.pontos)
       );
     });
