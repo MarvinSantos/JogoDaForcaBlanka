ad853d1669c56f4beae2fd2de376fc1d59e80272
Tiago Alves <stvtiago@gmail.com>
2015-10-24 21:14:09 -0300

    feat(home) feat(botoesHome): add Radio, e script do botao

--- js/botoesHome.js
+++ js/botoesHome.js
@@ -1,23 +1,36 @@
+$(function() {
 
-$(function(){
-  $('#submit').click(function(){
+  $('#submit').click(function() {
     var nome = $('input:text').val();
-    var normal = $('#normal').checked;
-    var nunes = $('#nunes').checked;
-    if(normal === true){
-      check = 'normal';
-    }else{
-      check = 'nunes';
+    var normal = $("input[value='Normal']:checked").val();
+    var nunes = $("input[value='Nunes']:checked").val();
+    var urlPessoas = 'http://localhost:3000/pessoas';
+
+    if(consultarPessoas(nome) > 0){
+        if(normal.valueOf() === 'Normal'){
+          $.post(urlPessoas, { nome: nome, pontos: 0, dificuldade: normal });
+        }else if(nunes.valueOf() === 'Nunes'){
+          $.post(urlPessoas, { nome: nome, pontos: 0, dificuldade: nunes });
+        }
     }
-    adicionarJSON(nome, check);
+
+    $(function(){
+      location.href = 'file:///C:/Users/vicente/Documents/JogoDaForcaBlanka/tela-jogo.html';
+      console.log('redirecionando...');
+    })
+
   });
 
-    function adicionarJSON(nome, check) {
-      $.get('localhost:3000').done(function(data){
-        var pessoas = data.pessoas[{nome: nome pontos: 0 dificuldade: check];
-      })
-    });
-  });
+  function consultarPessoas(nome) {
+    var cont = 0;
+    $.get(urlPessoas).done(function(pessoas) {
+      pessoas.forEach(function(elem){
+        if(nome !== elem.nome){
+          cont++;
+        }
+      });
+    })
+      return cont;
+  }
 
-
-});
+})
