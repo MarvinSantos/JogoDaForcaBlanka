f18620d90fc5f1afae36ebb60c1f063b60ff7c38
Marvin Santos <marvincenteno23@gmail.com>
2015-10-26 00:32:04 -0200

    ajustando gravar banco

--- js/teclado.js
+++ js/teclado.js
@@ -177,29 +177,3 @@ function mostraLetraErrada(letter){
     $('<li>').html('-  '+letter +'  -').addClass('inline')
   )
 };
-
-function gravarBanco() {
-  currentPoints = 0;
-  var nome = $('input:text').val();
-  if(nome !== ''){
-    var normal = $("input[value='Normal']:checked").val();
-    var nunes = $("input[value='Nunes']:checked").val();
-    var urlPessoas = 'http://localhost:3000/pessoas';
-
-    localStorage["pessoas"] = JSON.stringify({
-      "nome": nome,
-      "pontos": 0
-    });
-
-
-    if(normal === 'Normal'){
-      $.post(urlPessoas, { nome: nome, pontos: 0, dificuldade: normal }).done(function(){
-        irParaTelaJogo();
-      });
-    }else if(nunes === 'Nunes'){
-      $.post(urlPessoas, { nome: nome, pontos: 0, dificuldade: nunes }).done(function(){
-        irParaTelaJogo();
-      });
-    }
-  }
-};
